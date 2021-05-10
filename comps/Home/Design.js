50b9d7a64086a0f5a7d44cf5ea68aa0408009bed
DevVatu <devvatu@gmail.com>
2021-05-10 16:41:36 +0200

    just started out

---
+++ comps/Home/Design.js
@@ -0,0 +1,14 @@
+import styles from "../../styles/Home/Design.module.css"
+
+export default function Design() {
+  return (
+    <div className={styles.design}>
+      <div className={styles.obj} id={styles.obj1}></div>      
+      <div className={styles.obj} id={styles.obj2}></div>      
+      <div className={styles.obj} id={styles.obj3}></div>      
+      <div className={styles.obj} id={styles.obj4}></div>      
+      <div className={styles.obj} id={styles.obj5}></div>      
+      <div className={styles.obj} id={styles.obj6}></div>      
+    </div>
+  )
+}
